e522916c5ee7d59a44c9d0639af59af86bb67c25
SurajKale01 <surajkale9140@gmail.com>
2025-05-07 23:03:26 +0530

    Add bcrypt, jsonwebtoken, and mongoose dependencies to the backend

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connetToDb = require('./db/db');
+
+connetToDb();
 
 
 app.use(cors());
