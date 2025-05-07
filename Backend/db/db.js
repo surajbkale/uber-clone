9895319a4d1c1a5eeacaf5002b9dbc4cc4692c4a
SurajKale01 <surajkale9140@gmail.com>
2025-05-07 23:03:26 +0530

    Add bcrypt, jsonwebtoken, and mongoose dependencies to the backend

---
+++ Backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(process.env.DB_CONNECT)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+module.exports = connectToDb;
