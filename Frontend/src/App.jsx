d4d6ab000bc14bb17b9cf10baab65ed5ffe0a0ac
SurajKale01 <surajkale9140@gmail.com>
2025-05-10 23:44:46 +0530

    Add user authentication features and update routing
    - Create Start page as the landing page
    - Implement UserLogout component for user logout functionality
    - Add UserProtectWrapper for protecting routes
    - Update UserLogin and UserSignup components to handle user authentication with API calls
    - Modify UserContext to use object for user state management
    - Add axios dependency for making HTTP requests
    - Update Home page to a simple placeholder
    - Update routing in App component to reflect new structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,39 @@
 import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Start from "./pages/Start.jsx";
 import UserLogin from "./pages/UserLogin.jsx";
 import UserSignup from "./pages/UserSignup.jsx";
-import CaptainLogin from "./pages/CaptainLogin.jsx";
+import Captainlogin from "./pages/Captainlogin.jsx";
 import CaptainSignup from "./pages/CaptainSignup.jsx";
+import Home from "./pages/Home.jsx";
+import UserProtectWrapper from "./pages/UserProtectWrapper.jsx";
+import UserLogout from "./pages/UserLogout.jsx";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
-        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-login" element={<Captainlogin />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/user/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
       </Routes>
     </div>
   );
