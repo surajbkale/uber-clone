aee2c18c8aedbeb81a1bd0e8d6bfc62377d7df18
SurajKale01 <surajkale9140@gmail.com>
2025-05-11 16:06:01 +0530

    Add ride management features: implement ride creation, model, and routes; enhance captain experience with riding and finish ride components; update frontend navigation and styling.

--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { CaptainDataContext } from "../context/CaptainContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 const CaptainSignup = () => {
   const navigate = useNavigate();
 
@@ -187,7 +186,7 @@ const CaptainSignup = () => {
         </p>
       </div>
       <div>
-        <p className="text-[10px] leading-tight">
+        <p className="text-[10px] mt-6 leading-tight">
           This site is protected by reCAPTCHA and the{" "}
           <span className="underline">Google Privacy Policy</span> and{" "}
           <span className="underline">Terms of Service apply</span>.
